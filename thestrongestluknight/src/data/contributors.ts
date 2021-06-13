4508c529adaba586f3150c27d4d4c23e1142330e
alnj <kokone@takat.su>
2021-06-13 17:06:18 +0200

    contributors

---
+++ thestrongestluknight/src/data/contributors.ts
@@ -0,0 +1,68 @@
+import { Map } from 'immutable'
+
+export const credits: Map<ContributorCategory, Contributor[]> = Map({
+  Organization: [
+    {
+      name: 'tam（タム）', url: 'https://twitter.com/tam_vaccount',
+      details: 'Project leader, internal translation'
+    }, {
+      name: 'みらい', url: 'https://twitter.com/Mirai_Malekith/',
+      details: 'Internal translation'
+    }, {
+      name: 'こーでー', url: 'https://twitter.com/kody_lu',
+      details: 'Internal translation'
+    }, {
+      name: 'いいおか', url: 'https://twitter.com/iiokayama8929',
+      details: 'Internal translation'
+    },
+  ],
+  Website: [
+    { name: 'tam（タム）', url: 'https://twitter.com/tam_vaccount' },
+    { name: 'monoAI', url: 'https://twitter.com/monoai_' }
+  ]
+  Artwork: [
+    // Please input their name (Japanese preferred), and Twitter URL
+    // If no Twitter URL then Pixiv URL
+
+    // [OK!!!] Coco with Luna back: [TAIWAN]
+    // https://www.pixiv.net/en/artworks/79038147
+
+    // [OK!!!] Cozy Gen 4 [EN] [REQUESTED ON TWITTER]
+    // https://www.pixiv.net/en/artworks/89126212
+    // https://www.pixiv.net/en/artworks/86730174 <= need to ask this too
+
+    // [OK!!!] chibi yonksei [JP] [PRELIMINARY DM REQUEST ON TWITTER]
+    // https://www.pixiv.net/en/artworks/89232325
+
+    // [OK!!!] Pixel art yonksei [JP]
+    // https://www.pixiv.net/en/artworks/88113908
+
+  ],
+  Clipping: [
+    { name: 'Cain Chin', url: 'https://twitter.com/cain_chin' },
+    // Please include the clipping channels' names and their Twitter URL
+    // if no Twitter URL then their YouTube ch URL
+  ],
+  'HimeCoco playlist': [
+    { name: 'Cain Chin', url: 'https://twitter.com/cain_chin' },
+    { name: 'Marshall Lee', url: 'https://twitter.com/Marshall_Lee_A' },
+  ]
+})
+
+///////////////////////////////////////////////////////////////////////////////
+
+const contributorCategories = [
+  'Artwork',
+  'Organization',
+  'Website',
+  'Clipping',
+  'HimeCoco playlist'
+] as const
+
+type ContributorCategory = typeof contributorCategories[number]
+
+interface Contributor {
+  name: string,
+  url?: string,
+  details?: string
+}
